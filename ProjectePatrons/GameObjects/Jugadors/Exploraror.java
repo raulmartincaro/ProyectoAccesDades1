354fc898e4e6f5d2c2bcb5d3cbc2028783f6ce78
raulmartincaro <raul.martincaro@gmail.com>
2023-10-23 16:12:54 +0200

    cosas de pol

---
+++ ProjectePatrons/GameObjects/Jugadors/Exploraror.java
@@ -0,0 +1,12 @@
+package Jugadors;
+
+public class Exploraror extends Jugador {
+
+	/*
+	 * El constructor tipo Exploraror que extiende de la clase abstracta jugador
+	 */
+	public Exploraror(String nom, int puntuacio, int saldo) {
+		super(nom, puntuacio, saldo);
+	}
+
+}
