107f35bb10c7715971c04eedacfe0153b20907d7
Pchaozz <polsh2001.1@gmail.com>
2023-10-23 17:36:16 +0200

    Ahora el jugador es una interficie
    - Pues eso que ahora es una inteficie shavales 👌

--- ProjectePatrons/GameObjects/Jugadors/Comerciant.java
+++ ProjectePatrons/GameObjects/Jugadors/Comerciant.java
@@ -1,12 +1,76 @@
 package Jugadors;
 
-public class Comerciant extends Jugador {
+public class Comerciant implements Jugador {
+	
+	//VARIABLES CLASE
+	String nom;
+	int puntuacio;
+	int saldo;
+	private TipusJugador tipus = TipusJugador.Comerciant;
 
 	/*
 	 * El constructor tipo Comerciant que extiende de la clase abstracta jugador
 	 */
 	public Comerciant(String nom, int puntuacio, int saldo) {
-		super(nom, puntuacio, saldo);
+		this.nom = nom;
+		this.saldo = saldo;
+		this.puntuacio = puntuacio;
+	}
+	
+	/* 
+	 * GETTERS AND SETTERS
+	 */
+
+	public String getNom() {
+		return nom;
 	}
 
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+
+	public int getPuntuacio() {
+		return puntuacio;
+	}
+
+
+
+	public void setPuntuacio(int puntuacio) {
+		this.puntuacio = puntuacio;
+	}
+
+
+
+	public int getSaldo() {
+		return saldo;
+	}
+
+
+
+	public void setSaldo(int saldo) {
+		this.saldo = saldo;
+	}
+
+	/*
+	 * Metodo que devuelve que tipo de jugador es
+	 * @return El tipus de jugador que es
+	 */
+
+	@Override
+	public TipusJugador GetTipus() {
+		return tipus;
+	}
+	
+	/*
+	 * Metodo to string que devuelve un string con toda la informacion
+	 * @return Un string con toda la infomacion del objeto
+	 */
+	@Override
+	public String toString() {
+		return "Exploraror [nom=" + nom + ", puntuacio=" + puntuacio + ", saldo=" + saldo + ", tipus=" + tipus + "]";
+	}
 }
--- ProjectePatrons/GameObjects/Jugadors/Exploraror.java
+++ ProjectePatrons/GameObjects/Jugadors/Exploraror.java
@@ -1,12 +1,75 @@
 package Jugadors;
 
-public class Exploraror extends Jugador {
+public class Exploraror implements Jugador {
 
+	//VARIABLES CLASE
+	String nom;
+	int puntuacio;
+	int saldo;
+	TipusJugador tipus = TipusJugador.Explorador;
 	/*
-	 * El constructor tipo Exploraror que extiende de la clase abstracta jugador
+	 * El constructor tipo Comerciant que extiende de la clase abstracta jugador
 	 */
 	public Exploraror(String nom, int puntuacio, int saldo) {
-		super(nom, puntuacio, saldo);
+		this.nom = nom;
+		this.saldo = saldo;
+		this.puntuacio = puntuacio;
+	}
+	
+	/* 
+	 * GETTERS AND SETTERS
+	 */
+
+	public String getNom() {
+		return nom;
 	}
 
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+
+	public int getPuntuacio() {
+		return puntuacio;
+	}
+
+
+
+	public void setPuntuacio(int puntuacio) {
+		this.puntuacio = puntuacio;
+	}
+
+
+
+	public int getSaldo() {
+		return saldo;
+	}
+
+
+
+	public void setSaldo(int saldo) {
+		this.saldo = saldo;
+	}
+
+	/*
+	 * @return El tipus de jugador que es
+	 */
+
+	@Override
+	public TipusJugador GetTipus() {
+		return tipus;
+	
+	}
+
+	/*
+	 * Metodo to string que devuelve un string con toda la informacion
+	 * @return Un string con toda la infomacion del objeto
+	 */
+	@Override
+	public String toString() {
+		return "Exploraror [nom=" + nom + ", puntuacio=" + puntuacio + ", saldo=" + saldo + ", tipus=" + tipus + "]";
+	}
 }
